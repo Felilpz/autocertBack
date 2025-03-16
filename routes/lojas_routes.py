70091a90d854fc093e4fe89ab60c6cd7bba79573
Felilpz <luizfelipe.vieira12@gmail.com>
2025-03-15 23:10:26 -0300

    routes done.

---
+++ routes/lojas_routes.py
@@ -0,0 +1,48 @@
+from flask import Blueprint, jsonify, request
+from models.loja import Loja
+from models.responsavel import Responsavel
+from models.loja_responsavel import lojaResponsavel
+from db.connection import db
+
+loja_routes = Blueprint('loja_routes', __name__)
+
+
+@loja_routes.route('/lojas', methods=['GET'])
+def get_lojas():
+    lojas = Loja.query.all()
+    return jsonify([{
+        'id': loja.id,
+        'cnpj': loja.cnpj,
+        'razaosocial': loja.razaosocial,
+        'bandeira': loja.bandeira,
+        'validade_certificado': loja.validade_certificado
+    } for loja in lojas])
+
+
+@loja_routes.route('/responsaveis', methods=['GET'])
+def get_responsaveis():
+    responsaveis = Responsavel.query.all()
+    return jsonify([{
+        'id': responsavel.id,
+        'nome': responsavel.nome,
+        'telefone': responsavel.telefone,
+        'email': responsavel.email
+    } for responsavel in responsaveis])
+
+
+@loja_routes.route('/associar_responsavel', methods=['POST'])
+def associar_responsavel():
+    loja_id = request.json.get('loja_id')
+    responsavel_id = request.json.get('responsavel_id')
+
+    loja = Loja.query.get(loja_id)
+    responsavel = Responsavel.query.get(responsavel_id)
+
+    if loja and responsavel:
+        loja_responsavel = lojaResponsavel(  # agora usando o nome da classe importada
+            loja_id=loja.id, responsavel_id=responsavel.id)
+        db.session.add(loja_responsavel)
+        db.session.commit()
+        return jsonify({'message': 'responsavel associado à loja com sucesso!'}), 201
+
+    return jsonify({'message': 'loja ou responsavel não encontrados!'}), 404
